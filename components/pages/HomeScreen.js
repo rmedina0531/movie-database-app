647c356475791c5fa1187bdc4a7709d5bbb9f0d6
Ricardo Medina <rmedina0531@gmail.com>
2021-04-04 13:42:37 -0700

    Moved all components inside of page due to scope of function. Working

--- components/pages/HomeScreen.js
+++ components/pages/HomeScreen.js
@@ -1,16 +1,79 @@
-import React from "react";
-import { Button, View, Text, StyleSheet } from "react-native";
-import Search from "../Search";
+import React, { useState } from "react";
+import { Button, View, Text, StyleSheet, TextInput } from "react-native";
+// import Search from "../Search";
+
+const SearchEntry = (props) => {
+  return (
+    <View style={styles.searchEntry}>
+      <Text style={{ paddingRight: 5 }}>{props.type}:</Text>
+      <TextInput
+        style={styles.textBox}
+        onChangeText={(text) => {
+          props.onChange(text);
+          console.log(props.value);
+        }}
+        // value={workMinutes.toString()}
+      />
+    </View>
+  );
+};
+
+const Search = (props) => {
+  return (
+    <View style={styles.container}>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <SearchEntry
+        type="Title"
+        onChange={props.titleHandler}
+        value={props.title}
+      />
+      <SearchEntry
+        type="Year"
+        onChange={props.yearHandler}
+        value={props.year}
+      />
+      <Button title="Search" onPress={props.onSearch} />
+    </View>
+  );
+};
 
 const HomeScreen = ({ navigation }) => {
+  let [title, setTitle] = useState("title");
+  let [year, setYear] = useState("year");
+
+  const titleHandler = (text) => {
+    setTitle(text);
+  };
+  const yearHandler = (text) => {
+    setYear(text);
+  };
+
+  async function searchButtonHandler() {
+    let titleSearch = "s=" + title;
+    let plot = "plot=full";
+    let url = "http://www.omdbapi.com/?";
+    let apiKey = "apikey=2abc6af7";
+    const res = await fetch(url + apiKey + "&" + titleSearch);
+
+    const data = await res.json();
+    console.log(data);
+    navigation.navigate("Profile");
+    // console.log(title, year);
+  }
   return (
     <View style={styles.homeScreen}>
       <Text style={styles.h1}>Movie Database Search App</Text>
-      <Search />
-      <Button
+      <Search
+        onSearch={searchButtonHandler}
+        title={title}
+        year={year}
+        titleHandler={titleHandler}
+        yearHandler={yearHandler}
+      />
+      {/* <Button
         title="Go to Profile"
         onPress={() => navigation.navigate("Profile")}
-      />
+      /> */}
     </View>
   );
 };
@@ -27,5 +90,16 @@ const styles = StyleSheet.create({
   },
   h1: {
     fontSize: 30,
+  },
+  searchEntry: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  textBox: {
+    borderColor: "gray",
+    borderWidth: 1,
+    backgroundColor: "white",
+    fontSize: 15,
+    width: 50,
   },
 });
